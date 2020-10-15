abeb73215b6646c6637632ff2adc3899f11ab8a0
Bruno Gonçalves <brunomgoncalves@gmail.com>
2020-10-15 17:30:23 -0300

    Initial commit

---
+++ Alura.WebAPI.Api/Modelos/LivroPaginacao.cs
@@ -0,0 +1,51 @@
+﻿using Alura.ListaLeitura.Modelos;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Dynamic.Core;
+using System.Threading.Tasks;
+
+namespace Alura.WebAPI.Api.Modelos
+{
+
+    public static class LivroPaginacaoExtensions
+    {
+        public static LivroPaginado ToLivroPaginado(this IQueryable<LivroApi> query, LivroPaginacao paginacao)
+        {
+            int totalItens = query.Count();
+            int totalPaginas = (int)Math.Ceiling(totalItens / (double)paginacao.Tamanho); //Pegar o maior valor na divisão do total de paginas 
+
+            return new LivroPaginado 
+            { 
+                Total = totalItens,
+                TotalPaginas = totalPaginas,
+                NumeroPagina = paginacao.Pagina,
+                TamanhoPagina = paginacao.Tamanho,
+                Resultado = query.Skip(paginacao.Tamanho * (paginacao.Pagina - 1))
+                                 .Take(paginacao.Tamanho).ToList(),
+                Anterior = (paginacao.Pagina > 1) ? $"livros?tamanho={paginacao.Pagina - 1}&pagina={paginacao.Tamanho}":"",
+                Proximo = (paginacao.Pagina < totalPaginas)? $"livros?tamanho={paginacao.Pagina + 1}&pagina={paginacao.Tamanho}":""
+            };
+        }
+    }
+
+    public class LivroPaginado
+    {
+        public int Total { get; set; }
+        public int TotalPaginas { get; set; }
+        public int TamanhoPagina { get; set; }
+        public int NumeroPagina { get; set; }
+        public IList<LivroApi> Resultado { get; set; }
+        public string Anterior { get; set; }
+        public string Proximo { get; set; }
+    }
+
+
+
+    public class LivroPaginacao
+    {
+        public int Tamanho { get; set; } = 1;
+        public int Pagina { get; set; } = 25;
+    }
+}
---
+++ Alura.WebAPI.Api/Startup.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Alura.ListaLeitura.Api.Formatters;
+using Alura.ListaLeitura.Modelos;
+using Alura.ListaLeitura.Persistencia;
+using Alura.WebAPI.Api.Filtros;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Versioning;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+
+namespace Alura.WebAPI.Api
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration config)
+        {
+            Configuration = config;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<LeituraContext>(options => {
+                options.UseSqlServer(Configuration.GetConnectionString("ListaLeitura"));
+            });
+
+            services.AddTransient<IRepository<Livro>, RepositorioBaseEF<Livro>>();
+
+            services.AddMvc(options => {
+                options.OutputFormatters.Add(new LivroCsvFormatter());
+                options.Filters.Add(typeof(ErrorResponseFilter));
+            }).AddXmlSerializerFormatters();
+
+            //Desabilita filtros do ModelState para customizar o retorno de erros deste tipo na API
+            services.Configure<ApiBehaviorOptions>(options => 
+            {
+                options.SuppressModelStateInvalidFilter = true;
+            });
+
+
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = "JwtBearer";
+                options.DefaultChallengeScheme = "JwtBearer";
+            }).AddJwtBearer("JwtBearer", options => {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes("alura-webapi-authentication-valid")),
+                    ClockSkew = TimeSpan.FromMinutes(5),
+                    ValidIssuer = "Alura.WebApp",
+                    ValidAudience = "Postman",
+                };
+            });
+
+
+            //Versionamento por URL
+            services.AddApiVersioning();
+
+            /*
+             * Versionamento por query string ou cabeçalho da requisicao
+                services.AddApiVersioning(options =>
+                {
+                    options.ApiVersionReader = ApiVersionReader.Combine(
+                        new QueryStringApiVersionReader("api-version"), //usada na requisicao via query string uri?api-version=1.0
+                        new HeaderApiVersionReader("api-version") //Usada na requisicao no cabeçalho na key api-version => 1.0
+                        );
+                });
+            */
+
+
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseAuthentication();
+
+            app.UseMvc();
+        }
+    }
+}
